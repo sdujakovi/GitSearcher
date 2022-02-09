4112c11208ac97ce939e3fd643b4efe0be228403
sdujakovi <sdujakovi@foi.hr>
2022-02-09 22:25:38 +0100

    Uređivanje komentara.

--- app/src/main/java/com/example/gitsearcher/interfaces/IGitRepositoryModel.kt
+++ app/src/main/java/com/example/gitsearcher/interfaces/IGitRepositoryModel.kt
@@ -2,7 +2,7 @@ package com.example.gitsearcher.interfaces
 
 import com.example.gitsearcher.model.data.GitRepositoryHeader
 
+//implementirano u model.service.IService
 interface IGitRepositoryModel {
-    //implementirano u model.service.IService
-    fun getGitRepository() : GitRepositoryHeader
+    fun getGitRepository()
 }
